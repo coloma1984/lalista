652091f084b8e196b2c27aae96628da322b1e85f
Fernando Coloma <64309728+coloma1984@users.noreply.github.com>
2020-06-16 15:18:54 +0200

    Maquetacion lista, botones y texto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'lalista-ng';
+  mediasub: Subscription;
+  deviceXs: boolean;
+  deviceSm: boolean;
+  deviceMd: boolean;
 
-  constructor() { 
+  constructor(public mediaObserver:MediaObserver) { 
+  }
+  ngOnInit(): void {
 
+    this.mediasub = this.mediaObserver.media$.subscribe(
+      (result: MediaChange) => {
+        console.log(result.mqAlias);
+        this.deviceXs = result.mqAlias === 'xs' ? true : false;
+        this.deviceSm = result.mqAlias === 'sm' ? true : false;
+        this.deviceMd = result.mqAlias === 'md' ? true : false;
+      }
+    )
+  }
+  ngOnDestroy(): void {
+    this.mediasub.unsubscribe();
   }
 
   reloadPage(): void{
